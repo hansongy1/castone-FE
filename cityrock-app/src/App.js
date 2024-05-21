b79d6998188e0ae225205521b3438fb66270ea60
songyi <allrohsy@gmail.com>
2024-05-22 02:16:21 +0900

    main.css 추가 - 가운데 정렬되도록 폭 고정

--- cityrock-app/src/App.js
+++ cityrock-app/src/App.js
@@ -1,18 +1,22 @@
 /* App.js */
-import { BrowserView, MobileView } from 'react-device-detect'
+import React from 'react';
+// import { BrowserView, MobileView } from 'react-device-detect' /* 브라우저, 모바일 */
 import Header from "./components/Header";
+import './styles/main.css'; /* 가운데 정렬 */
 
 function App() {
   return (
-    <section className='contents'>
-      <Header />
-      <BrowserView>
-        브라우저 뷰입니다.
-      </BrowserView>
-      <MobileView>
-        <p>모바일뷰입니다.</p>
-      </MobileView>
-    </section>
+    <div className='container'>
+      <section className='contents'>
+        <Header />
+        {/* <BrowserView>
+          브라우저 뷰입니다.
+        </BrowserView>
+        <MobileView>
+          <p>모바일뷰입니다.</p>
+        </MobileView> */}
+      </section>
+    </div>
   );
 }
 
