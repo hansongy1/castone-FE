79e0da6aeeb879cbaad1b3a078d1e1585afe5e7c
songyi <allrohsy@gmail.com>
2024-05-30 17:43:13 +0900

    로그인 수정

--- cityrock-app/src/pages/Login.jsx
+++ cityrock-app/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 // Login.jsx
 import React from "react";
 import '../styles/Login.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons"
 
 const Login = () => {
     // const [email, setEmail] = useState("");
@@ -12,10 +14,17 @@ const Login = () => {
             <form action="post">
                 <input type="text" name="email" placeholder="이메일"/>
                 <input type="password" name="password" placeholder="비밀번호" />
+                <FontAwesomeIcon icon={faEye} />
+                
                 <label htmlFor="remember-check">
                     <input type="checkbox" id="remeber-check" />
-                    <p>비밀번호 기억하기</p>
+                    <p>로그인 상태 유지</p>
                 </label>
+                <section className="find-container">
+                    <p>이메일/비밀번호 찾기</p>
+                    <div className="find-line"></div>
+                    <p>회원가입</p>
+                </section>
                 <button type="submit">로그인</button>
             </form>
         </div>
