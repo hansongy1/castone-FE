d0a190de94fc3360799cb2374e585dcc7f669fc5
songyi <allrohsy@gmail.com>
2024-05-30 20:24:45 +0900

    초기 사용자 알고리즘 파일 생성

--- cityrock-app/src/App.js
+++ cityrock-app/src/App.js
@@ -1,12 +1,12 @@
 /* App.js */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
-// import { BrowserView, MobileView } from 'react-device-detect' /* 브라우저, 모바일 */
 import Header from "./components/Header";
 import './styles/main.css'; /* 가운데 정렬 */
 import Login from "./pages/Login";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Register';
+import InitialUser from "./pages/InitialUser";
 
 function App() {
   return (
@@ -25,6 +25,7 @@ function App() {
             <Route path="/" element={<Home /> } />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/initialUser" element={<InitialUser />} />
           </Routes>
         </section>
       </div>
