60437cec4c273c669ef788b6f226fab24340ec01
songyi <allrohsy@gmail.com>
2024-07-29 17:56:31 +0900

    추천 아이콘 추가

--- cityrock-app/src/components/Home.js
+++ cityrock-app/src/components/Home.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
 import '../styles/main.css'; // 가운데 정렬
 
+import Favorite from "../assets/icon_favorite_border.png";// 클릭 전 아이콘
+import Favorite_Fill from "../assets/icon_favorite_fill.png"; // 클릭 후 아이콘
+import Ex from "../assets/festival2.png";
+
 const CAROUSEL_IMAGES = [
     'https://kfescdn.visitkorea.or.kr/kfes/upload/contents/db/fe1f7525-8451-4fdf-9df6-eb99d5997be6_2.jpg',
     'https://kfescdn.visitkorea.or.kr/kfes/upload/contents/db/202a3575-509f-4e34-bbf8-59328d7b89eb_2.jpg',
@@ -9,9 +13,9 @@ const CAROUSEL_IMAGES = [
 
 const Carousel = ({ carouselList }) => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const startX = useRef(0); // 추가된 부분
-    const currentX = useRef(0); // 추가된 부분
-    const isDragging = useRef(false); // 추가된 부분
+    const startX = useRef(0);
+    const currentX = useRef(0);
+    const isDragging = useRef(false);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -39,7 +43,7 @@ const Carousel = ({ carouselList }) => {
     const handleMouseUp = () => {
         if (!isDragging.current) return;
         isDragging.current = false;
-        if (currentX.current = startX.current > 50) {
+        if (currentX.current - startX.current > 50) {
             // 오른쪽에서 왼쪽으로 슬라이드
             setCurrentImageIndex((prevIndex) => (prevIndex - 1 + carouselList.length) % carouselList.length);
         } else if (startX.current - currentX.current > 50) {
@@ -70,6 +74,15 @@ const Carousel = ({ carouselList }) => {
 };
 
 const Home = () => {
+    // favorite icon
+    const [favorite, setFavorite] = useState([false, false, false]); // 1. favorite 상태 초기값 설정
+
+    const toggleFavorite = (index) => { // 2. toggleFavorite 함수 추가
+        const newFavorite = [...favorite];
+        newFavorite[index] = !newFavorite[index];
+        setFavorite(newFavorite);
+    };
+
     return (
         <section className="home-contents">
             <h1 className="home-p">New</h1>
@@ -85,21 +98,21 @@ const Home = () => {
                     <div className="c-img">
                         <img src="" alt="" />
                     </div>
-                    <div className="etc-text">
+                    <div className="etc-text-1">
                         <p className="text-t">화려한 조명, 멋진 조명<br />그리고 열정적인 아티스트</p>
                         <p className="link-t">공연 바로가기</p>
                     </div>
                 </div>
                 <div className="cate-box">
                     <div className="c-img" />
-                    <div className="etc-text">
+                    <div className="etc-text-2">
                         <p className="text-t">현대적이고 창의적인</p>
                         <p className="link-t">전시회 바로가기</p>
                     </div>
                 </div>
                 <div className="cate-box">
                     <div className="c-img" />
-                    <div className="etc-text">
+                    <div className="etc-text-2">
                         <p className="text-t">다채로운 문화, 함성과 기쁨</p>
                         <p className="link-t">축제 바로가기</p>
                     </div>
@@ -110,11 +123,39 @@ const Home = () => {
                     <h1 className="home-p">추천</h1>
                     <div className="makdagi-2" />
                 </div>
-                <p>당신에게 알맞은 컨텐츠를 추천해드릴게요!</p>
-                <div className="rec-alg">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                <div className='reco-contents'>
+                    <p className='reco-text'>당신에게 알맞은 컨텐츠를 추천해드릴게요!</p>
+                    <div className="reco-alg">
+                        <div className='alg-contents'>
+                            <div className='alg-box'>
+                                <img src={Ex} alt="" className='alg-img'/>
+                                <img 
+                                    src={favorite[0] ? Favorite_Fill : Favorite} // 3. 상태에 따른 아이콘 설정
+                                    alt='Favorite'
+                                    className='alg-icon'
+                                    onClick={() => toggleFavorite(0)} // 4. onClick 이벤트 핸들러 추가
+                                />
+                            </div>
+                            <p className='alg-title'>곡성세계장미축제</p>
+                            <p className='alg-loc'>전라남도 곡성군</p>
+                        </div>
+                        <div className='alg-contents'>
+                            <div className='alg-box'>
+                                <img src="" alt="" className='alg-img'/>
+                                <img src={Favorite} alt='Favorite' className='alg-icon'/>
+                            </div>
+                            <p className='alg-title'>명</p>
+                            <p className='alg-loc'>위치</p>
+                        </div>
+                        <div className='alg-contents'>
+                            <div className='alg-box'>
+                                <img src="" alt="" className='alg-img'/>
+                                <img src={Favorite} alt='Favorite' className='alg-icon'/>
+                            </div>
+                            <p className='alg-title'>명</p>
+                            <p className='alg-loc'>위치</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
