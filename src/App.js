31055029c4137c8e06a41b571a1dd3932eba6379
yujeong2947 <dlsbwjd12@naver.com>
2024-10-03 01:00:24 +0900

    Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/* App.js */
+import React from 'react';
+import './styles/main.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Mypage from './mypage';
+import LoginMypage from './loginmypage';
+import Profile from './profile';
+import Noticelist from './noticelist.js';
+import Notice1 from './notice1.js';
+import Notice2 from './notice2.js';
+import Notice3 from './notice3.js';
+import Notice4 from './notice4.js';
+import Review from './review.js';
+import Reviewlist from './reviewlist.js';
+import Reviewfav from './reviewfav.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='container'>
+        <section className='contents'>
+          <Routes>
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/loginmypage" element={<LoginMypage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/noticelist" element={<Noticelist />} />
+            <Route path="/notice1" element={<Notice1 />} />
+            <Route path="/notice2" element={<Notice2 />} />
+            <Route path="/notice3" element={<Notice3 />} />
+            <Route path="/notice4" element={<Notice4 />} />
+            <Route path="/review" element={<Review />} />
+            <Route path="/reviewlist" element={<Reviewlist />} />
+            <Route path="/reviewfav" element={<Reviewfav/>} />
+          </Routes>
+        </section>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/noticelist.js
@@ -0,0 +1,53 @@
+/* App.js */
+import React from 'react';
+// import { BrowserView, MobileView } from 'react-device-detect' /* 브라우저, 모바일 */
+import Header from "./components/Header";
+import './styles/main.css'; /* 가운데 정렬 */
+
+function App() {
+  return (
+    <div className='container'>
+      <section className='contents'>
+        <Header />
+        <div style={{ flexDirection: 'column', alignItems: 'center', minHeight: '100vh'}}>
+          <div style={{ fontSize: '20px', fontWeight: 'bold' }}>
+            <p style={{ textAlign: 'center' }}>공지사항</p>
+          </div>
+          <div style={{ width: '100%', height: '1px', backgroundColor: 'lightGray' }}></div>
+          <div style={{ fontSize: '14px', textIndent: '20px' }}>
+            <a href="http://localhost:3000/notice1" style={{ textDecoration: 'none', color: 'black' }}>
+              <p>축제 일정 안내</p>
+            </a>
+          </div>
+          <div style={{ width: '100%', height: '0.5px', backgroundColor: 'lightGray' }}></div>
+          <div style={{ fontSize: '14px', textIndent: '20px' }}>
+            <a href="http://localhost:3000/notice2" style={{ textDecoration: 'none', color: 'black' }}>
+              <p>사전 등록 이벤트</p>
+            </a>
+          </div>
+          <div style={{ width: '100%', height: '1px', backgroundColor: 'lightGray' }}></div>
+          <div style={{ fontSize: '14px', textIndent: '20px' }}>
+            <a href="http://localhost:3000/notice3" style={{ textDecoration: 'none', color: 'black' }}>
+              <p>주차 안내</p>
+            </a>
+          </div>
+          <div style={{ width: '100%', height: '0.5px', backgroundColor: 'lightGray' }}></div>
+          <div style={{ fontSize: '14px', textIndent: '20px' }}>
+            <a href="http://localhost:3000/notice4" style={{ textDecoration: 'none', color: 'black' }}>
+              <p>자원봉사자 모집</p>
+            </a>
+          </div>
+          <div style={{ width: '100%', height: '1px', backgroundColor: 'lightGray' }}></div>
+        </div>
+        {/* <BrowserView>
+          브라우저 뷰입니다.
+        </BrowserView>
+        <MobileView>
+          <p>모바일뷰입니다.</p>
+        </MobileView> */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
